5a5b4407b32cf3e0f5e3a8c0596dfca02002f5d5
halloween-bit16 <ruchi3110pawar@gmail.com>
2025-09-01 14:40:29 +0530

    changes

---
+++ pointers/pointers.c
@@ -0,0 +1,13 @@
+// notes to understand pointers in C    
+
+#include <stdio.h>
+
+void main(){
+    
+    int a = 20;
+    printf("address of a: %p\n", &a); 
+    // & is the address of operator 
+    printf("value of a: %d\n", a); 
+    
+    
+}
